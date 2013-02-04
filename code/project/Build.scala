6efd7c5ea932ed5eedac23d84312ffd4f46e7bc5
Dr. Erich W. Schreiner <eschreiner@yahoo.com>
2013-02-04 13:36:41 +0100

    organization added to Build descriptor

--- code/project/Build.scala
+++ code/project/Build.scala
@@ -13,13 +13,16 @@ object ApplicationBuild extends Build {
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+
+		organization := "com.sdc",
+
       resolvers += "jBCrypt Repository" at "http://repo1.maven.org/maven2/org/",
 
-      resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
-      resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns),
+//      resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/")),
+//      resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/")),
 
       libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.2.1",
-      libraryDependencies += "com.feth" %% "play-easymail" % "0.1-SNAPSHOT",
+//      libraryDependencies += "com.feth" %% "play-easymail" % "0.1-SNAPSHOT",
       libraryDependencies += "org.mindrot" % "jbcrypt" % "0.3m",
       libraryDependencies += "commons-lang" % "commons-lang" % "2.6"
     )
